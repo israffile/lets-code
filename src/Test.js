b6ed1cba38c5d3bd4f39f8826306dafa2f848e00
kazi md israfil <kazimdisrafil7@gmail.com>
2024-05-15 01:25:07 +0930

    button update

---
+++ src/Test.js
@@ -0,0 +1,22 @@
+
+
+class Note {
+    constructor(title, data) {
+        this.title = title;
+        this.data = data;
+    }
+}
+
+var n1 = Note("Note 1", "g dfsg dfg dfgd fg");
+
+var notes = [];
+notes.push(n1);
+
+
+
+
+export default function() {
+    return (
+        <div>Title</div>
+    );
+}
---
+++ test.js
@@ -0,0 +1,14 @@
+
+let myLlist = [];
+
+console.log(myLlist);
+
+myLlist.push(12);
+myLlist.push(13);
+myLlist.push(14);
+
+console.log(myLlist);
+
+myLlist.pop();
+
+console.log(myLlist);
